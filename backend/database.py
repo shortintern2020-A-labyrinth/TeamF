33c63b4c62d014601b1b069a824aaeb7ffd9ec88
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2020-08-24 10:23:58 +0900

    add method to create connection to mysql db

---
+++ backend/database.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+import sys
+
+db = SQLAlchemy()
+
+def init_db(app):
+  db.init_app(app)
+
+def test_connection():
+  ok = True
+  result = "ok"
+
+  try:
+    db.session.execute('SELECT 1')
+  except Exception as e:
+      output = str(e)
+      ok = False
+
+  return ok, output
