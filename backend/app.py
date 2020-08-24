9e4b1d1b7f99d694599a2408d811a21029dcc09b
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2020-08-24 16:15:47 +0900

    add logger

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,6 @@
+import logging
+import logging.handlers
+import datetime
 from flask import Flask, jsonify
 from .database import init_db, test_connection
 from .models import *
@@ -6,10 +9,21 @@ app=Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 app.config.from_object('app.config.Config')
 
+handler = logging.handlers.RotatingFileHandler(
+    f"logs/log_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.log", "a+", maxBytes=3000, backupCount=5)
+handler.setLevel(logging.WARN)
+handler.setFormatter(logging.Formatter(
+    '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'))
+logger = logging.getLogger('app')
+logger.addHandler(handler)
+
 init_db(app)
 
 @app.route('/')
 def index():
+  app.logger.warn('warn')
+  app.logger.error('error')
+  app.logger.critical('critical')
   return jsonify({
     "message": "test"
   })
