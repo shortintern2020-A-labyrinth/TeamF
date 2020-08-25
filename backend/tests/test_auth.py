c07d3e6e9e0704f1f827ce6949d79985b7e43410
Makoto Naruse <mako_marimo@yahoo.co.jp>
2020-08-25 14:39:55 +0900

    add checkAPI testing

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -102,4 +102,34 @@ class TestSigninAPI(BaseTestCase):
     # access_tokenがある
     assert response2.json["access_token"]
 
+class TestCheckAPI(BaseTestCase):
+  def test_no_authorization_header(self):
+    response = self.app.get('/protected')
+    self.assert_status(response, 401)
 
+  def test_with_authorization_header(self):
+    # まずはsignup
+    response1 = self.app.post(
+      '/signup',
+      data=json.dumps(dict(
+        email='example@example.com',
+        password='password',
+        user_name='test'
+      )),
+      content_type='application/json'
+    )
+    # createdが返ってくる
+    self.assert_status(response1, 201)
+    # access_tokenがある
+    assert response1.json["access_token"]
+
+    access_token = response1.json["access_token"]
+
+    response2 = self.app.get(
+      '/protected',
+      headers={
+        'Authorization': f"Bearer {access_token}"
+      }
+    )
+    # 認証されているので200が返る
+    self.assert_status(response2, 200)
