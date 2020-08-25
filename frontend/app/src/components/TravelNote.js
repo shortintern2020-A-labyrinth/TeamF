e29f20cfa1fbfee1c720002ea521c9a0f036fb02
mola1129 <ms.mola1129@gmail.com>
2020-08-25 09:02:25 +0900

    Fix some layouts.

--- frontend/app/src/components/TravelNote.js
+++ frontend/app/src/components/TravelNote.js
@@ -1,4 +1,4 @@
-// Author: Satoshi Moro
+// Editor: Satoshi Moro
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Card, Grid, Typography } from '@material-ui/core';
@@ -8,46 +8,49 @@ import RoomIcon from '@material-ui/icons/Room';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        padding: '15px',
+        margin: theme.spacing(4),
     },
     card: {
         display: 'flex',
     },
     image: {
         width: '30%',
-        padding: '10px',
+        margin: theme.spacing(2),
     },
     like: {
         display: 'flex',
     },
+    location: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
 }));
 
-export default function TravelNote({ title, description, place, likeNumber, userName }) {
+export default function TravelNote({ title, description, image, country, city, likeNumber, userName }) {
     const classes = useStyles();
 
     return (
         <div className={classes.root}>
             <Card className={classes.card} m={2}>
-                <img className={classes.image} src={NoImage} />
+                <img className={classes.image} src={image ? image : NoImage} alt="travel top" />
                 <Grid container justify="center" alignItems="center">
                     <Grid item xs={12}>
-                        <Typography component="div">{title ? title : 'タイトル'}</Typography>
+                        <Typography variant='h6'>{title ? title : 'タイトル'}</Typography>
                     </Grid>
                     <Grid item xs={10}>
-                        <p>{description ? description : '説明'}</p>
+                        <Typography>{description ? description : '説明'}</Typography>
                     </Grid>
                     <Grid item xs={2} className={classes.like} >
-                        <FavoriteIcon />
+                        <FavoriteIcon color="secondary" />
                         <span>{likeNumber ? likeNumber : 'いいね数'}</span>
                     </Grid>
-                    <Grid item xs={4}>
+                    <Grid item xs={8} className={classes.location}>
                         <RoomIcon />
+                        <Typography variant="body2">{country && city ? `${country} ${city}` : "国名都市名"}</Typography>
                     </Grid>
                     <Grid item xs={4}>
-                        <p>{place ? place : '場所'}</p>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <p>{userName ? userName : 'ユーザ名'}</p>
+                        <Typography variant="body2">{userName ? userName : 'ユーザ名'}</Typography>
                     </Grid>
                 </Grid>
             </Card>
--- frontend/app/src/pages/ListTravelNotes.js
+++ frontend/app/src/pages/ListTravelNotes.js
@@ -1,49 +1,68 @@
-import TravelNote from '../components/TravelNote.js';
+// Editor: Satoshi Moro
 import React from 'react';
-import Container from '@material-ui/core/Container';
+import TravelNote from '../components/TravelNote'
+import { Container, Typography, Divider, Box, IconButton } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const travelNotes = [
     {
-        id: '1',
+        id: 1,
         title: '千葉への旅行',
         description: '楽しかった',
-        place: '千葉県千葉市',
-        likeNumber: '6',
+        image: '',
+        country: '千葉県',
+        city: '千葉市',
+        startDate: '',
+        endDate: '',
+        // optional
+        likeNumber: 6,
         userName: '田中',
     },
     {
-        id: '2',
+        id: 2,
         title: '千葉への旅行',
         description: '楽しかった',
-        place: '千葉県千葉市',
-        likeNumber: '6',
+        image: '',
+        country: '千葉県',
+        city: '千葉市',
+        startDate: '',
+        endDate: '',
+        // optional
+        likeNumber: 7,
         userName: '田中',
     },
     {
-        id: '3',
+        id: 3,
         title: '千葉への旅行',
         description: '楽しかった',
-        place: '千葉県千葉市',
-        likeNumber: '6',
+        image: '',
+        country: '千葉県',
+        city: '千葉市',
+        startDate: '',
+        endDate: '',
+        // optional
+        likeNumber: 10,
         userName: '田中',
     },
 ];
 
 export default function ListTravelNotes() {
     return (
-        <Container maxWidth="sm">
-            <h1>旅行記一覧</h1>
-            {travelNotes.map(travelNote => (
-                <TravelNote
-                    key={travelNote.id}
-                    title={travelNote.title}
-                    description={travelNote.description}
-                    place={travelNote.place}
-                    likeNumber={travelNote.likeNumber}
-                    userName={travelNote.userName}
-                />
-            ))}
-        </Container>
+        <Box mt={10}>
+            <Container maxWidth="md">
+                <Typography variant="h4" align="left" >旅行記一覧</Typography>
+                <Divider />
+                {travelNotes.map(travelNote => (
+                    <TravelNote
+                        key={travelNote.id}
+                        {...travelNote}
+                    />
+                ))}
+                <IconButton aria-label="next">
+                    <ExpandMoreIcon />
+                </IconButton>
+            </Container>
+        </Box>
     );
 };
 
