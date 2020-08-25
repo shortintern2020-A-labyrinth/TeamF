c967cc7e9edc13e8bf07bdaeab5287e339dc87d3
KotaIkehara <ikehara.aurora95@gmail.com>
2020-08-25 09:43:50 +0900

    Design signup page

--- frontend/app/src/pages/Signup.js
+++ frontend/app/src/pages/Signup.js
@@ -1,5 +1,75 @@
+// Author: Kota Ikehara
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { Link } from 'react-router-dom';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Typography from '@material-ui/core/Typography';
+import { useStyles as LoginStyles } from './Login'
+import Container from '@material-ui/core/Container';
+import SNSLogin from '../components/SNSLogin';
 
-export default function Signup()  {
-    return <h1>Signup</h1>
+
+export default function Signup() {
+  const classes = LoginStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <AccountCircleIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          新規作成
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="ユーザ名"
+            name="name"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="メールアドレス"
+            name="email"
+            autoComplete="email"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="パスワード"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            アカウント作成
+          </Button>
+          <div>
+            <Link to="/Login" className={classes.link}>すでにアカウントをお持ちですか？</Link>
+          </div>
+        </form>
+        <SNSLogin />
+      </div>
+    </Container>
+  );
 }
