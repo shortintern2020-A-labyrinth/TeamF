6b7458d9f1d115b93f6f9cd31214150c116277e3
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2020-08-26 13:55:19 +0900

    rename path /comment/create -> /travel_note/<travel_note_id>/comment/create

--- backend/blueprints/comment.py
+++ backend/blueprints/comment.py
@@ -18,32 +18,31 @@ def index():
   })
 
 
-@comment.route('/comment/create', methods=["POST"])
+@comment.route('/travel_note/<travel_note_id>/comment/create', methods=["POST"])
 @jwt_required
-def create():
+def create(travel_note_id):
   if request.json is None:
-    return jsonify({"mode": "comment/create", "status": "bad_request", "message": "Please send json format"}), 400
+    return jsonify({"mode": "/travel_note/<travel_note_id>/cooment/create", "status": "bad_request", "message": "Please send json format"}), 400
 
-  if "travel_note_id" not in request.json:
-    return jsonify({"mode": "comment/create", "status": "bad_request", "message": "There are invalid parameters"}), 400
+  if "travel_note_id" is None:
+    return jsonify({"mode": "/travel_note/<travel_note_id>/cooment/create", "status": "bad_request", "message": "There are invalid parameters"}), 400
   if "body" not in request.json:
-    return jsonify({"mode": "comment/create", "status": "bad_request", "message": "There are invalid parameters"}), 400
+    return jsonify({"mode": "/travel_note/<travel_note_id>/cooment/create", "status": "bad_request", "message": "There are invalid parameters"}), 400
 
   # get params
-  travel_note_id = request.json["travel_note_id"]
   body = request.json["body"]
 
   if len(body) > 4095:
-    return jsonify({"mode": "comment/create", "status": "bad_request", "message": "Comment is too long"}), 400
+    return jsonify({"mode": "/travel_note/<travel_note_id>/cooment/create", "status": "bad_request", "message": "Comment is too long"}), 400
 
   # check travel note exists
   try:
     exists = TravelNote.query.get(travel_note_id)
     if not exists:
-      return jsonify({"mode": "comment/create", "status": "bad_request", "message": "Such travel note does not exist"}), 400
+      return jsonify({"mode": "/travel_note/<travel_note_id>/cooment/create", "status": "bad_request", "message": "Such travel note does not exist"}), 400
   except Exception as e:
     logger.warn(e)
-    return jsonify({"mode": "comment/create", "status": "internal_server_error", "message": "Internal server error"}), 500
+    return jsonify({"mode": "/travel_note/<travel_note_id>/cooment/create", "status": "internal_server_error", "message": "Internal server error"}), 500
 
   # get user_name
   user_id = get_jwt_identity()
@@ -52,7 +51,7 @@ def create():
         User.user_name).filter(User.id == user_id).one()
   except Exception as e:
     logger.warn(e)
-    return jsonify({"mode": "comment/create", "status": "internal_server_error", "message": "Internal server error"}), 500
+    return jsonify({"mode": "/travel_note/<travel_note_id>/cooment/create", "status": "internal_server_error", "message": "Internal server error"}), 500
 
   #insert Comment
   comment = Comment(user_id, travel_note_id, body, user_name, user_name)
