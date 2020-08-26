47d5b186a8deb89aba2b3bc67181ade0b8cc8686
KotaIkehara <ikehara.aurora95@gmail.com>
2020-08-26 17:25:31 +0900

    Add auth

---
+++ frontend/app/src/pages/Auth.js
@@ -0,0 +1,9 @@
+// Author: Kota Ikehara
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import User from './User';
+
+const Auth = (props) =>
+  User.isLoggedIn() ? props.children : <Redirect to="/login" />;
+
+export default Auth;
