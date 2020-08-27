b30046b7ad63a91c8a1a4ffa8d6f2ee69fc03d1b
KotaIkehara <ikehara.aurora95@gmail.com>
2020-08-27 09:27:03 +0900

    Fix auth

--- frontend/app/src/pages/Auth.js
+++ frontend/app/src/pages/Auth.js
@@ -1,9 +1,8 @@
 // Author: Kota Ikehara
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import User from './User';
 
 const Auth = (props) =>
-  User.isLoggedIn() ? props.children : <Redirect to="/login" />;
+  props.screenProps.loginStatus ? props.children : <Redirect to="/login" />;
 
 export default Auth;
